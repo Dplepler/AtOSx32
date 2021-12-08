0d2f893e7435159b40da1c1af63c9ab70a87c27d
Dplepler <david.plepler@gmail.com>
2021-12-09 00:11:57 +0200

    Fix readme

--- AtOSx64/Kernel/kernel.c
+++ AtOSx64/Kernel/kernel.c
@@ -1,11 +1,35 @@
+#define VIDEO_MEMORY 0xB8000
 
-#define VIDEO_MEMORY 0xb8000
-
-int kernel_start()
+void kernel_start()
 {
+	__asm__ __volatile__ (
+
+	"call main\n"
+	"jmp .\n"
+
+	);
+}
+
+
+int main()
+{
+
+
+	char* videoMem = (char*)VIDEO_MEMORY;
+	
+	*videoMem = 'Z';
+
+
+
+	
+	return 0;
+}
+
+void test()
+{
+	
+
 	char* videoMem = (char*)VIDEO_MEMORY;
 	
 	*videoMem = 'X';
-	
-	return 0;
 }
