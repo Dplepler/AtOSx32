7ff3ce789416cda35577e4882b31b7465eeb597a
Dplepler <david.plepler@gmail.com>
2022-05-22 10:50:11 -0700

    malloc!

--- AtOSx32/Kernel/heap.c
+++ AtOSx32/Kernel/heap.c
@@ -68,6 +68,8 @@ heap_header* heap_allocate_header(unsigned int size) {
 
 void heap_split_header(heap_header* header) {
 
+  if (heap_get_index(header->size - (header->req_size + sizeof(heap_header))) >= 0) { return; }   // Nothing to split
+
   heap_header* split_header = (heap_header*)((uint32_t)header + header->req_size + sizeof(heap_header));
   split_header->signature = HEAP_SIGNATURE;
   split_header->size = header->size - (header->req_size + sizeof(heap_header));   // Remainder size will now become the new header's size
@@ -102,5 +104,7 @@ void* malloc(size_t size) {
     used_pages[index] = true;
   }
 
+  heap_split_header(header); // If there's data that will never be used, split it to a new header
 
+  return (void*)((uint32_t)header + sizeof(heap_header));
 }
