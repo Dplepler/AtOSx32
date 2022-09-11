bafb79aa2df22d751b23b5800e1a1704e7434011
Dplepler <david.plepler@gmail.com>
2022-09-11 13:48:14 -0700

    Create task init

--- AtOSx32/Kernel/Process/process.c
+++ AtOSx32/Kernel/Process/process.c
@@ -20,6 +20,24 @@ void init_multitasking() {
   sti();
 }
 
+void create_task(uint8_t state, uint32_t* address_space) { 
+
+  cli();
+  
+  aprocess_t* current_proc = kmalloc(sizeof(aprocess_t));
+  
+  current_proc->state = state;
+  current_proc->address_space = address_space;
+  current_proc->pid = get_next_pid();
+  current_proc->esp0 = kmalloc(STACK_SIZE) + KERNEL_STACK - 0x4;   // Create new stack
+
+  current_proc->esp = current_proc->esp0 - 0x4;
+  
+
+  sti();
+}
+
+
 uint32_t get_next_pid() {
   static uint32_t next_pid = 8008;
   return next_pid++;
