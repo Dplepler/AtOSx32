6b1d6e3eb5c21a13d7bcfd7c8677f2a1f842d318
Dplepler <david.plepler@gmail.com>
2022-12-12 21:29:08 +0200

    Inode struct

--- AtOSx32/Kernel/fs/fs.h
+++ AtOSx32/Kernel/fs/fs.h
@@ -19,7 +19,21 @@
 
 #define FAT_SIGNATURE 0xA00A
 
+#define FILENAME_SIZE 8
+#define EXTENTION_SIZE 3
+
 #define get_next_cluster fat_extract_value
+
+typedef enum _ATTRIBUTE_FLAGS_ENUM {
+
+  ATTRIBUTE_READ = 0x0,
+  ATTRIBUTE_HIDDEN,
+  ATTRIBUTE_SYSTEM,
+  ATTRIBUTE_VOLUME_LABEL,
+  ATTRIBUTE_DIRECTORY,
+  ATTRIBUTE_ACHIEVE
+
+} attribute_t;
 
 void init_fat();
 void fat_setup_table();
@@ -27,11 +41,29 @@ uint16_t fat_extract_value(uint16_t index);
 
 typedef struct _INODE_ENTRY_STRUCT {
 
+  char filename[FILENAME_SIZE];
+  char ext[EXTENTION_SIZE];
+
+  attribute_t attribute;
+
+  uint8_t reserved1;
+
+  uint8_t creation;
   
+  uint16_t creation_time;
+  uint16_t creation_date;
+  uint16_t last_access;
 
+  uint16_t reserved2;
 
+  uint16_t last_write_time;
+  uint16_t last_write_date;
 
-} inode_t;
+  uint16_t cluster;
+
+  uint32_t size;
+
+} __attribute__((packed)) inode_t;
 
 
 
